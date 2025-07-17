e3b7b5dd8739ccfc900fa832cae9b9839d911788
lawrencebelle <lawrence.belle@atomicmedia.co.uk>
2025-07-17 14:25:07 +0100

    configured scraper

--- scraper.py
+++ scraper.py
@@ -1,10 +1,52 @@
 from bs4 import BeautifulSoup
+import requests
+from playwright.sync_api import sync_playwright
+import tldextract
+from pathlib import Path
 
-baseUrl = "https://www.bbc.co.uk/"
+URL = "https://www.bbc.co.uk/"
+DIRECTORY_ROOT= "page_data"
 
 
-if '__name__' == '__main__':
-    with open(baseUrl) as fp:
-        soup = BeautifulSoup(fp)
+def get_page_info(url: str) -> None:
+    file_directory_base = f"{DIRECTORY_ROOT}/{tldextract.extract(url).domain}"
+    Path(file_directory_base).mkdir(parents=True, exist_ok=True)
+    scrape_page(url, file_directory_base)
+    with sync_playwright() as playwright:
+        take_screenshot(playwright, url, file_directory_base)
 
-        print(soup.contents)
+
+def take_screenshot(playwright, url: str, file_directory_base: str) -> None:
+    browser = playwright.chromium.launch()
+    page = browser.new_page()
+    page.goto(url)
+    page.screenshot(path=f"{file_directory_base}/screenshot.png", full_page=True)
+    browser.close()
+
+def scrape_page(url: str, file_directory_base: str) -> None:
+    response = requests.get(URL)
+    html_content = response.text
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    html = get_html(soup)
+    with open(f"{file_directory_base}/html.txt", "w") as text_file:
+        text_file.write(html)
+
+
+def get_html(soup: BeautifulSoup) -> str:
+    return soup.prettify()
+
+
+# def get_css(soup: BeautifulSoup) -> list[str]:
+#     css_files = []
+#     for css in soup.find_all("link"):
+#         if css.attrs.get("href"):
+#             css_url = urljoin(URL, css.attrs.get("href"))
+#             css_files.append(css_url)
+#     print(css_files)
+#     return css_files
+
+
+if __name__ == '__main__':
+    get_page_info(URL)
